fefce8a4cc274bd84a81d2b840d98425316511ea
Vitor Fraga <vitorfragaps@gmail.com>
2025-06-11 22:30:08 -0300

    fix: standardize background color casing and improve formatting in Select component

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
-export const Select =  styled.select`
-  background-color: #FFF;
+export const Select = styled.select`
+  background-color: #fff;
   height: 52px;
   box-shadow: 0px, 4px, 10px rgba(0, 0, 0, 0.04);
   border-radius: 4px;
@@ -14,10 +14,11 @@ export const Select =  styled.select`
   font-size: 16px;
 
   border: 2px solid transparent;
+  appearance: none;
 
   width: 100%;
 
   &:focus {
-    border: 2px solid ${({theme}) => theme.colors.primary.main};
+    border: 2px solid ${({ theme }) => theme.colors.primary.main};
   }
-`
+`;
